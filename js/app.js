777f8ac432e382082d0b37b2daa7f42d58c13d02
skybaros <rostyslavsmile@gmail.com>
2015-12-17 03:21:19 +0200

    Lesson 1 js

---
+++ js/app.js
@@ -0,0 +1,6 @@
+var name, time, place, event;
+name = "Ros";
+time = "19:00";
+place = "Lviv, Comein";
+event = prompt ("");
+var sentense = console.log(name + " " + time + " " + place + " " + event); //краще оголосити змінну тут, чи рядком вище і окремо?
